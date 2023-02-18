71e28b7570381f5127424cb8357a26447828f93f
kumkumkl <118705690+kumkumkl@users.noreply.github.com>
2023-02-19 01:06:16 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins This is PES1UG20CS184\n";
+  cout << "This in pipeline\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
